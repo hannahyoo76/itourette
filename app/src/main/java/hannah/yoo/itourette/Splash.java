babb147eb30d7db92ab028a161b8186e0a0d9321
Hannah Yoo <hannahyoo@Hannahs-MacBook-Air.local>
2021-12-31 13:09:43 +0900

    ky

--- app/src/main/java/hannah/yoo/itourette/Splash.java
+++ app/src/main/java/hannah/yoo/itourette/Splash.java
@@ -23,7 +23,7 @@ public class Splash extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3000);
+        },2000);
     }
     @Override
     protected void onPause() {
