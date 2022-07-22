5e939b847d2dbae7ef7d742886cea891cbd8554d
hannahyoo <hannahyoo76@gmail.com>
2022-07-22 13:57:32 +0900

    hn

--- app/src/main/java/hannah/yoo/itourette/FocusTracker.java
+++ app/src/main/java/hannah/yoo/itourette/FocusTracker.java
@@ -4,12 +4,22 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FocusTracker extends AppCompatActivity {
     MediaPlayer audio;
+    private FirebaseAuth mAuth;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,6 +30,7 @@ public class FocusTracker extends AppCompatActivity {
         audio = MediaPlayer.create(FocusTracker.this,R.raw.d1); // 일단은 d1 임시로 해놨어요
 
         Button buttonStart = findViewById(R.id.buttonStart);
+        ImageButton trackButton = findViewById(R.id.trackbutton);
 
         // Visibility invisible 로 바꾸기 once button is clicked
         buttonStart.setOnClickListener(new View.OnClickListener() {
@@ -31,10 +42,25 @@ public class FocusTracker extends AppCompatActivity {
 
 
         //1. firebase 코드 여기서 불러오기
-        //2.
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://itourette-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = database.getReference("user/Level/");
+
+        //2. tracker 하기 (first example)
+        Timer m_timer = new Timer();
+        TimerTask m_task = new TimerTask() {
+            @Override
+            public void run() {
+                trackButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // flag?
+                    }
+                });
+            }
+        };
+        m_timer.schedule(m_task, 1000);
 
     }
-
 
     public void PlayIT(View v) {
         audio.start();
